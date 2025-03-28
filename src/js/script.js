6b739302e90e3340838a5c7f7e3bef3d3f7c581d
WhiteMooncy <167587307+WhiteMooncy@users.noreply.github.com>
2025-03-27 23:27:30 -0300

    update menu need fix
    Error in the second nav, it does not stay fixed when scrolling

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,19 @@ document.addEventListener('DOMContentLoaded', function() {
             navbar.classList.add('initial');
         }
     });
+
+    // Seleccionar la segunda barra de navegación
+    const navbarPlatos = document.getElementById('navbar-platos');
+
+    // Obtener la posición inicial de la barra de navegación
+    const navbarOffsetTop = navbarPlatos.offsetTop;
+
+    // Agregar un evento de scroll
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= navbarOffsetTop) {
+            navbarPlatos.classList.add('sticky'); // Agregar clase sticky
+        } else {
+            navbarPlatos.classList.remove('sticky'); // Quitar clase sticky
+        }
+    });
 });
